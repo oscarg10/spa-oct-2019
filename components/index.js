0f8558d7392989eaeec34148386dfdfc0898af0c
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-06 19:36:15 -0600

    Import and export all components

---
+++ components/index.js
@@ -0,0 +1,8 @@
+/**
+ *  Export whatever comes out as the default from Header.js as 'Header'
+ * All of this will be exported out as an ES Module with the given keys
+ */
+export { default as Header } from "./Header";
+export { default as Nav } from "./Nav";
+export { default as Main } from "./Main";
+export { default as Footer } from "./Footer";
---
+++ index.js
@@ -0,0 +1,4 @@
+// 'Destructoring' each of the named components from the components directories.
+import { Header, Nav, Main, Footer } from "./components";
+
+console.log(Header, Nav, Main, Footer);
