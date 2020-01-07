b8953959c45573d03dc58bac5ed9a85639654bf1
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-07 11:00:01 -0600

    Render initial SPA and convert to functional components

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 // 'Destructoring' each of the named components from the components directories.
 import { Header, Nav, Main, Footer } from "./components";
 
-console.log(Header, Nav, Main, Footer);
+const state = {
+  Home: {
+    //This is just going to be a string that gets passed into our Header component
+    heading: "Home"
+  },
+  Form: {
+    //This is just going to be a string that gets passed into our Header component
+    heading: "Form"
+  },
+  Blog: {
+    heading: "Blog"
+  },
+  Gallery: {
+    heading: "Gallery"
+  }
+};
+//Query the document using a CSS selector
+document.querySelector("#root").innerHTML = `
+${Header()}
+${Nav()}
+${Main()}
+${Footer()}
+`;
