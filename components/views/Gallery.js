35387ff62777dc7c67a4009b874f4a002b6c7246
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-07 21:25:43 -0600

    Add markup in each one of the files

---
+++ components/views/Gallery.js
@@ -0,0 +1,42 @@
+export default `
+<div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some header</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some Smaml Test</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+      <div>
+        <figure><img src="https://source.unsplash.com/random/800x600" alt="" /></figure>
+        <h2>Some Heading</h2>
+        <p>Some small text</p>
+      </div>
+`;
---
+++ components/views/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Blog } from "./Blog";
+export { default as Gallery } from "./Gallery";
+export { default as Contact } from "./Contact";
