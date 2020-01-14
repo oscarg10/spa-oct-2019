bb81fd78b6237834650641b9b4458a68ccd1d857
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-13 19:52:25 -0600

    Add function to import data

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -1,5 +1,18 @@
+function createArticleHTML(posts) {
+  return posts
+    .map(post => {
+      return `
+  <article>
+    <h2>${post.title}</h2>
+      <p>${post.body}</p>
+  </article>`;
+    })
+    .join(" ");
+}
+
 export default st => {
   return `
+  ${createArticleHTML(st.posts)}
 <p>Loading posts</p>
 `;
 };
